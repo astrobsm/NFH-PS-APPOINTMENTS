1e95e7757287c4f85b3bb68a7bad4c1f93570d2d
Dr Nnadi <sylvia4douglas@gmail.com>
2026-03-22 18:03:40 +0100

    Remove Other from gender options - Male/Female only

--- frontend/src/pages/BookAppointment.jsx
+++ frontend/src/pages/BookAppointment.jsx
@@ -189,7 +189,6 @@ export default function BookAppointment() {
                     <option value="">-- Select --</option>
                     <option value="Male">Male</option>
                     <option value="Female">Female</option>
-                    <option value="Other">Other</option>
                   </select>
                 </div>
               </div>
